8810b8db0786c81486f24ff5cad54fbb5ccf2f6c
Nahuel Lemes <nahulem@gmail.com>
2025-01-20 12:11:53 -0300

    feat(components): create Header Component

---
+++ src/app/components/header-actions/header-actions.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LanguageMenuComponent } from "../language-menu/language-menu.component";
+
+@Component({
+  selector: 'app-header-actions',
+  imports: [CommonModule, LanguageMenuComponent],
+  templateUrl: './header-actions.component.html',
+  styleUrl: './header-actions.component.scss'
+})
+export class HeaderActionsComponent {
+  public items: string[] = ["English", "French"];
+
+  public openLanguageMenu: boolean = false;
+  @ViewChild('headerLanguageSelector', { static: true }) languageSelector!: ElementRef<HTMLElement>;
+
+  isLanguageMenuExpanded = false;
+  
+  public toggleLanguageMenu(event: Event): void {
+    
+    event.preventDefault();
+    this.isLanguageMenuExpanded = !this.isLanguageMenuExpanded;
+    this.languageSelector.nativeElement.setAttribute('aria-expanded', this.isLanguageMenuExpanded.toString());
+  }
+
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
+import { HeaderActionsComponent } from '../header-actions/header-actions.component';
 
 @Component({
   selector: 'app-header',
-  imports: [MenuComponent],
+  imports: [MenuComponent, HeaderActionsComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
