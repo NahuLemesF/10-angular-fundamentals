a62afded34527a075da102e3587115fe60ff98e0
Nahuel Lemes <nahulem@gmail.com>
2025-01-20 15:03:44 -0300

    fix(components): repair Player Section errors

--- src/app/components/player-section/player-section.component.ts
+++ src/app/components/player-section/player-section.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-player-section',
   imports: [],
   templateUrl: './player-section.component.html',
-  styleUrl: './player-section.component.scss'
+  styleUrl: './player-section.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class PlayerSectionComponent {
 
